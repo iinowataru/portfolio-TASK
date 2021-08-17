97aeb4c8c9d53ca8c106dcb15eb09288d419b07a
iinowataru <wpfujz@gmail.com>
2021-08-17 11:47:34 +0000

    search

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,25 @@
+class SearchsController < ApplicationController
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+
+private
+ def search_for(model, content, method)
+   if model == 'item'
+    if mothod == 'perfect'
+     Item.where(name: content)
+    else
+    Item.where('name LIKE ?', '%'+content+'%')
+    end
+   elsif model == 'genre'
+     if method == 'perfect'
+      Genre.where(name: content)
+     else
+      Genre.where('name LIKE ?', '%'+content+'%')
+     end
+   end
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   #devise_for :customers
   root to: 'homes#top'
+  get '/search', to: 'searchs#search'
   
   resources :customers, only: [:index, :show, :edit, :update]
   resources :items, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :posts, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :genres, only: [:index, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
